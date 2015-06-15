46cf223ef2534978915b559dd33d7a39e7f2acc5
Wesley Smith <wesleyfsmith@gmail.com>
2015-06-14 19:43:12 -0600

    Adding foundation for profile page.

--- pages/profilePage/profilePage.js
+++ pages/profilePage/profilePage.js
@@ -2,3 +2,22 @@
  * Created by wesley on 6/3/15.
  */
 
+if (Meteor.isClient) {
+
+    Navbar.add({
+        url: '/profile',
+        menuName: 'Account',
+        menuOrientation: 'right'
+    });
+
+    Router.route('/profile', function() {
+
+        if (!Meteor.user()) {
+            return;
+        }
+        this.render('profilePage');
+
+    });
+
+
+}
