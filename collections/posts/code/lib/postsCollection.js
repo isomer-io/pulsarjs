b7b624ae7b532ef67950075cdf544e474befbefe
Max Johansen <maxjohansen@berkeley.edu>
2015-06-15 19:35:32 -0600

    add beta stripe (no transaction history ui)

--- collections/posts/code/lib/postsCollection.js
+++ collections/posts/code/lib/postsCollection.js
@@ -66,7 +66,13 @@ Posts.attachSchema(new SimpleSchema({
 
     createdAt: orion.attribute('createdAt'),
 
-    updatedAt: orion.attribute('updatedAt')
+    updatedAt: orion.attribute('updatedAt'),
+
+    price:{
+        type:Number,
+        decimal:true,
+        min:0
+    }
 
 }));
 
