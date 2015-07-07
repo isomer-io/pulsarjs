b085bb9c838bc942ed91494a0f84a5c4afb42f7a
wesley <wesleyfsmith@gmail.com>
2015-07-06 21:06:51 -0600

    Moved navbar to package

---
+++ packages/pulsarjs-navbar/package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'wesleyfsmith:pulsarjs-navbar',
+  version: '0.0.1',
+  summary: '',
+  documentation: ''
+});
+
+Package.onUse(function(api) {
+  api.use(["templating",'tracker','session'], "client");
+
+  api.use("meteorhacks:async@1.0.0", "server");
+
+  api.use([
+    "orionjs:core@1.1.0",
+    "iron:router@1.0.9",
+    "meteorhacks:async@1.0.0",
+    "spacebars","ongoworks:security","nicolaslopezj:roles", 'wesleyfsmith:pulsarjs-config'
+  ], ["client", "server"]);
+
+  api.versionsFrom('METEOR@1.1.0.2');
+
+  //REVIEWS
+  api.addFiles(['lib/client/loadingbar.css'], 'client');
+  api.addFiles(['lib/client/navbar.html'], 'client');
+  api.addFiles(['lib/client/navbar.js'], 'client');
+  api.addFiles(['lib/server/navdict.js'], 'server');
+
+  api.export('Navbar');
+
+
+});
