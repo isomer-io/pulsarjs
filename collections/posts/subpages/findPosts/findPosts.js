e17a0e3c50662bacf8abd21132e1396585109116
wesley <wesleyfsmith@gmail.com>
2015-08-01 16:55:45 -0600

    Adjust pagination functionality

--- collections/posts/subpages/findPosts/findPosts.js
+++ collections/posts/subpages/findPosts/findPosts.js
@@ -1,67 +1,31 @@
+Posts.paginations = [];
 
-var createPage = function() {
-
-  /*
-  This is our pagination object. It lets us do an infinite
-  scroll through our list. You probably don't want to change
-  this unless you know what you are doing.
-   */
-
-   //create array here
-   if (!Posts.paginations) {
-     Posts.paginations = [];
-   }
-
-   Posts.paginations.push(new Meteor.Pagination(Posts, {
+Posts.paginations.push(new Meteor.Pagination(Posts, {
        infinite: true,
        itemTemplate: 'postInFindList',
+       templateName: "posts",
        sort: {
            createdAt: -1
        },
        availableSettings: {
-           sort: true
+           sort: true,
+           filter: true
        },
        fastRender: true
-   }));
-
-   console.log('server called');
-
-   return Posts.paginations.length - 1;
-
-}
-
-if(Meteor.isServer){
-
-  Meteor.methods({
-    initPagination:function(){
-
-      createPage();
-
-    }
-  });
-  
-}
+}));
 
 if (Meteor.isClient) {
 
     Template.findPosts.onCreated(function() {
 
-      var index = 0;
+      Tracker.autorun(function(){
 
-        Meteor.call('initPagination', function() {
-          index = createPage();
-
-          Tracker.autorun(function(){
-
-            Posts.paginations[index].set({
-              sort: Template.currentData().sort || {},
-              filter: Template.currentData().filter || {}
-            });
-
-          });
-
+        Posts.paginations[0].set({
+          sort: Template.currentData().sort || {},
+          filter: Template.currentData().filter || {}
         });
-
     });
+  });
+
 
 }
