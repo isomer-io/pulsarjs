bc41041e695cc607b53cadc2ef1c614ae6dee562
Max Johansen <maxjohansen@berkeley.edu>
2015-08-13 12:28:27 -0600

    switch to ES

--- collections/jobs/subpages/findJobs/findJobs.js
+++ collections/jobs/subpages/findJobs/findJobs.js
@@ -1,18 +1,34 @@
-Jobs.paginations = [];
+// Jobs.paginations = [];
+//
+// Jobs.paginations.push(new Meteor.Pagination(Jobs, {
+//        infinite: true,
+//        itemTemplate: 'jobInFindList',
+//        templateName: "jobs",
+//        sort: {
+//            createdAt: -1
+//        },
+//        availableSettings: {
+//            sort: true,
+//            filter: true
+//        },
+//        filters:{
+//          title:{
+//            $text: {$search: 'Test'}
+//          }
+//        },
+//        fastRender: true
+// }));
 
-Jobs.paginations.push(new Meteor.Pagination(Jobs, {
-       infinite: true,
-       itemTemplate: 'jobInFindList',
-       templateName: "jobs",
-       sort: {
-           createdAt: -1
-       },
-       availableSettings: {
-           sort: true,
-           filter: true
-       },
-       fastRender: true
-}));
+// if(Meteor.isServer){
+//   Jobs.paginations[0].set({
+//     auth:function(){
+//       // var results = EasySearch.search("Test");
+//       // console.log(results);
+//     }
+//   });
+// }
+
+Jobs.initEasySearch('title');
 
 if (Meteor.isClient) {
 
@@ -20,10 +36,10 @@ if (Meteor.isClient) {
 
       Tracker.autorun(function(){
 
-        Jobs.paginations[0].set({
-          sort: Template.currentData().sort || {},
-          filter: Template.currentData().filter || {}
-        });
+        // Jobs.paginations[0].set({
+        //   sort: Template.currentData().sort || {},
+        //   filter: Template.currentData().filter || {}
+        // });
     });
   });
 
